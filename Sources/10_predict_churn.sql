dd878c3614c501fdae8769a6334de38c8fc67538
RIN <128571571+RINz-HCMUS@users.noreply.github.com>
2025-04-12 18:00:19 +0700

    Update Predict
    fix ""Operation cannot be performed on a nested schema: Field: predicted_label_probs""

--- Sources/10_predict_churn.sql
+++ Sources/10_predict_churn.sql
@@ -1,31 +1,24 @@
 CREATE OR REPLACE TABLE project2_dataset.churn_predictions AS
 SELECT
-  *,
   predicted_label,
-  predicted_label_probs[OFFSET(1)].prob AS predicted_churn_probability
-FROM
-  ML.PREDICT(MODEL `project2_dataset.customer_churn_model`,
-    (
-      SELECT
-        gender,
-        SeniorCitizen,
-        Partner,
-        Dependents,
-        tenure,
-        PhoneService,
-        MultipleLines,
-        InternetService,
-        OnlineSecurity,
-        OnlineBackup,
-        DeviceProtection,
-        TechSupport,
-        StreamingTV,
-        StreamingMovies,
-        Contract,
-        PaperlessBilling,
-        PaymentMethod,
-        MonthlyCharges,
-        TotalCharges
-      FROM project2_dataset.processed_customer_churn
-    )
-  );
+  predicted_label_probs[OFFSET(1)].prob AS predicted_churn_probability,
+  gender,
+  SeniorCitizen,
+  Partner,
+  Dependents,
+  tenure,
+  PhoneService,
+  MultipleLines,
+  InternetService,
+  OnlineSecurity,
+  OnlineBackup,
+  DeviceProtection,
+  TechSupport,
+  StreamingTV,
+  StreamingMovies,
+  Contract,
+  PaperlessBilling,
+  PaymentMethod,
+  MonthlyCharges,
+  TotalCharges
+FROM project2_dataset.churn_predictions;
